cf4c027aa8d2afa1414a5e27addf4ef86e843caf
wituts <gusti.tut@gmail.com>
2026-03-06 21:37:16 +0800

    Merubah SKU dari 20 ke 100

--- database/migrations/2026_02_04_000011_create_products_table.php
+++ database/migrations/2026_02_04_000011_create_products_table.php
@@ -4,12 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('sku', 20)->unique();
+            $table->string('sku', 100)->unique();
             $table->string('name', 200);
             $table->text('description')->nullable();
             $table->foreignId('category_id')->constrained()->onDelete('restrict');
